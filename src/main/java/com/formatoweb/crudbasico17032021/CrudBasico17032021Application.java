af04284ec6c359a47f1969a157c679252a84cea2
GeorgeX <jorge@gmail.com>
2021-03-17 19:15:16 -0600

    Primera Carga

---
+++ src/main/java/com/formatoweb/crudbasico17032021/CrudBasico17032021Application.java
@@ -0,0 +1,13 @@
+package com.formatoweb.crudbasico17032021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBasico17032021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudBasico17032021Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/formatoweb/crudbasico17032021/CrudBasico17032021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formatoweb.crudbasico17032021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBasico17032021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
